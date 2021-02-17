ad8469b8f341a22fb608f6cf4de1e07d0c4276a6
Gisiora Elvis <gisioraelvis20@gmail.com>
2021-02-17 16:28:44 +0300

    mongodb models and routing

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,20 +1,21 @@
-import mongoose from 'mongoose'
+import path from "path";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+dotenv.config();
 
-const connectDB = async () =>{
-try{
+const connectDB = async () => {
+  try {
     const conn = await mongoose.connect(process.env.MONGO_URI, {
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
-        useFindAndModify: false,
-        useCreateIndex: true
-      })
-      console.log(`MongoDB connected on host:${conn.connection.host}`)
-}
-catch(error){
-  console.error(`Error:${error.message}`);
-  process.exit(1);
-}
-
-}
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useFindAndModify: false,
+      useCreateIndex: true,
+    });
+    console.log(`MongoDB Connected: ${conn.connection.host}`.cyan.underline);
+  } catch (error) {
+    console.error(`Error:${error.message}`);
+    process.exit(1);
+  }
+};
 
 export default connectDB;
--- backend/server.js
+++ backend/server.js
@@ -1,25 +1,46 @@
+import path from "path";
 import express from "express";
-import connectDB from './config/db.js'
-import products from "./data/products.js";
 import dotenv from "dotenv";
+import colors from "colors";
+import morgan from "morgan";
+import connectDB from "./config/db.js";
+
+import productRoutes from "./routes/productRoutes.js";
+import { notFound, errorHandler } from "./middlewalers/errorMiddleware.js";
+
+dotenv.config();
+
+connectDB();
 
 const app = express();
-dotenv.config();
-connectDB()
 
+if (process.env.NODE_ENV === "development") {
+  app.use(morgan("dev"));
+}
+
+app.use(express.json());
+
+app.use("/api/products", productRoutes);
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "/frontend/build")));
+
+  app.get("*", (req, res) =>
+    res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"))
+  );
+} else {
+  app.get("/", (req, res) => {
+    res.send("API is running....");
+  });
+}
+
+app.use(notFound);
+app.use(errorHandler);
 const PORT = process.env.PORT || 5000;
+
 app.listen(
   PORT,
   console.log(
-    `Server running in ${process.env.NODE_ENV} mode on port ${PORT}...`
+    `Server running in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow.bold
   )
 );
-
-app.get("/api/products", (req, res) => {
-  res.json(products);
-});
-
-app.get("/api/products/:id", (req, res) => {
-  const product = products.find((p) => p._id === req.params.id);
-  res.json(product);
-});
